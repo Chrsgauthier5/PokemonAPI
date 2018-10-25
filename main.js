1559beb6a75715b8cd4bd5ca2145ea65d9937087
Chris Gauthier <chrsgauthier5@gmail.com>
2018-10-25 14:57:05 -0400

    mostly working API HW

--- main.js
+++ main.js
@@ -1,14 +1,43 @@
 $(document).ready(function () {
 
     
-    var topics = ["Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise",
-        "Caterpie", "Metapod", "Butterfree", "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto", "Pidgeot", "Rattata", "Raticate"];
-
+    var topics = ["bulbasaur", "ivysaur", "venusaur", "charmander", "charmeleon", "charizard", "squirtle", "wartortle", "blastoise"];
+    
+    
     for (i = 0; i < topics.length; i++) {
-        $("#buttons").append($("<button>", { id: "pokebuttons", class: "btn btn-info" }).text(topics[i]))
+        $("#buttons").append($("<button>", { id: "pokebuttons", class: "btn btn-dark" }).text(topics[i].toLowerCase()))
     }
 
-    $(".btn-info").click(function(){
+    $("#searchGifs").click(function () {
+        var input = $("#searchText").val().toLowerCase();
+        console.log(input)
+        if (input === ""){
+            alert ("Please enter a Pokemon");
+            return
+        }
+
+        if (topics.indexOf(input) !==-1){
+
+            alert ("There's already a GIPHY button for " + input);
+            return
+        }
+
+        console.log(input);
+        topics.push(input);  // add the input to the topics array
+        $("#buttons").append($("<button>", {class: "btn btn-dark" }).text(topics[topics.length - 1])); //create button for the topic searched in search bar
+    });
+
+    $("#searchText").keyup(function (event) { // enter key triggers click function above to add new box
+        console.log(event);
+        if (event.keyCode === 13){
+            $("#searchGifs").click()
+        }
+    });
+
+
+
+    $(document).on("click",".btn-dark", function(){
+        $("#gifsDisplay").empty()
         console.log("hi");
         inputSearch = this.textContent;
         console.log(inputSearch);
@@ -18,21 +47,18 @@ $(document).ready(function () {
             url: xhr,
             method: "GET"
         }).then(function(response){
-            console.log(response);
-        })
+            console.log("is this getting called")
+            
+            var gifs = response.data
+
+            for (i in gifs)
+            {$("#gifsDisplay").append("<img src='" + gifs[i].images.original.url + "' style='height:250px; width: 250px;'>")
+            }           
+        });
 
     })
 
 
-    $("#searchGifs").click(function () {
-        var input = $("#searchText").val();
-        console.log(input);
-        topics.push(input);  // add the input to the topics array
-        $("#buttons").append($("<button>", { id: "pokebuttons", class: "btn btn-info" }).text(topics[topics.length - 1])); //create button for the topic searched in search bar
-
-
-
-    });
 
 
 
